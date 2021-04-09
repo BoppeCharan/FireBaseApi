b4e987eda8b2dc4920c50265cfd3c10ed8b34aa4
Bharath Rohith Partha Charipally <30441890+bharathrohith@users.noreply.github.com>
2021-04-09 15:35:21 +0530

    Commented Auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,27 +5,27 @@ import { DataStoredInToken, RequestWithUser } from '../interfaces/auth.interface
 import userModel from '../models/users.model';
 
 const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
-  try {
-    const cookies = req.cookies;
+//   try {
+//     const cookies = req.cookies;
 
-    if (cookies && cookies.Authorization) {
-      const secret = process.env.JWT_SECRET;
-      const verificationResponse = (await jwt.verify(cookies.Authorization, secret)) as DataStoredInToken;
-      const userId = verificationResponse.id;
-      const findUser = userModel.find(user => user.id === userId);
+//     if (cookies && cookies.Authorization) {
+//       const secret = process.env.JWT_SECRET;
+//       const verificationResponse = (await jwt.verify(cookies.Authorization, secret)) as DataStoredInToken;
+//       const userId = verificationResponse.id;
+//       const findUser = userModel.find(user => user.id === userId);
 
-      if (findUser) {
-        req.user = findUser;
-        next();
-      } else {
-        next(new HttpException(401, 'Wrong authentication token'));
-      }
-    } else {
-      next(new HttpException(404, 'Authentication token missing'));
-    }
-  } catch (error) {
-    next(new HttpException(401, 'Wrong authentication token'));
-  }
+//       if (findUser) {
+//         req.user = findUser;
+//         next();
+//       } else {
+//         next(new HttpException(401, 'Wrong authentication token'));
+//       }
+//     } else {
+//       next(new HttpException(404, 'Authentication token missing'));
+//     }
+//   } catch (error) {
+//     next(new HttpException(401, 'Wrong authentication token'));
+//   }
 };
 
 export default authMiddleware;
